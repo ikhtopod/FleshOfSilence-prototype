f689f85cd97e1a2695db5375e533cdbce5c172c1
insoLLLent <vitaly@lifanoff.ru>
2020-06-02 23:06:47 +0300

    added project files

---
+++ Source/TheFleshOfSilence/TheFleshOfSilenceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TheFleshOfSilenceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEFLESHOFSILENCE_API ATheFleshOfSilenceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
